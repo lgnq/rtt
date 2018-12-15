fce8d279362937866446bb36466194d084ed8b9d
lgnq <dzzxzz@gmail.com>
2018-12-15 17:21:10 +0800

    Update rthw.h
    using const

--- include/rthw.h
+++ include/rthw.h
@@ -71,7 +71,7 @@ struct rt_irq_desc
 void rt_hw_interrupt_init(void);
 void rt_hw_interrupt_mask(int vector);
 void rt_hw_interrupt_umask(int vector);
-rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler, void *param, char *name);
+rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler, void *param, const char *name);
 
 rt_base_t rt_hw_interrupt_disable(void);
 void rt_hw_interrupt_enable(rt_base_t level);
