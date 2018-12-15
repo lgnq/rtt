283e783a76bf402a6d39220c6ca7ba2e7506885d
lgnq <dzzxzz@gmail.com>
2018-12-15 16:06:36 +0800

    Update rthw.h
    format code

--- include/rthw.h
+++ include/rthw.h
@@ -47,10 +47,7 @@ rt_base_t rt_hw_cpu_dcache_status(void);
 void rt_hw_cpu_reset(void);
 void rt_hw_cpu_shutdown(void);
 
-rt_uint8_t *rt_hw_stack_init(void       *entry,
-                             void       *parameter,
-                             rt_uint8_t *stack_addr,
-                             void       *exit);
+rt_uint8_t *rt_hw_stack_init(void *entry, void *parameter, rt_uint8_t *stack_addr, void *exit);
 
 /*
  * Interrupt handler definition
@@ -74,10 +71,7 @@ struct rt_irq_desc
 void rt_hw_interrupt_init(void);
 void rt_hw_interrupt_mask(int vector);
 void rt_hw_interrupt_umask(int vector);
-rt_isr_handler_t rt_hw_interrupt_install(int              vector,
-                                         rt_isr_handler_t handler,
-                                         void            *param,
-                                         char            *name);
+rt_isr_handler_t rt_hw_interrupt_install(int vector, rt_isr_handler_t handler, void *param, char *name);
 
 rt_base_t rt_hw_interrupt_disable(void);
 void rt_hw_interrupt_enable(rt_base_t level);
