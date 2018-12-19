c5b4887d34c423a79b40522e7619750335f56ba7
lgnq <dzzxzz@gmail.com>
2018-12-19 15:25:50 +0800

    Update rtdef.h
    format code

--- include/rtdef.h
+++ include/rtdef.h
@@ -797,7 +797,6 @@ enum rt_device_class_type
 #define RT_DEVICE_CTRL_RTC_GET_ALARM    0x12            /**< get alarm */
 #define RT_DEVICE_CTRL_RTC_SET_ALARM    0x13            /**< set alarm */
 
-typedef struct rt_device *rt_device_t;
 /**
  * Device structure
  */
@@ -826,6 +825,7 @@ struct rt_device
 
     void                     *user_data;                /**< device private data */
 };
+typedef struct rt_device *rt_device_t;
 
 /**
  * block device geometry structure
